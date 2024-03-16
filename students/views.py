90b70200c7299af70e6d4b7d550fa73ead163e16
msdsn <devsanms@outlook.com>
2024-03-16 16:47:08 +0300

    Context kullan

--- students/views.py
+++ students/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'students/home.html')
+    context = {
+        'title': 'Home',
+    }
+    return render(request, 'students/home.html', context)
     # return HttpResponse('Hello, world!')
