4b4b91c97c8e7c760ac8db8841f1e28b8f7183c5
msdsn <devsanms@outlook.com>
2024-03-16 16:26:37 +0300

    Render ile html sayfası döndür

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import home
 
 urlpatterns = [
-    path('', index),
+    path('', home),
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('Hello, world!')
+def home(request):
+    return render(request, 'home.html')
+    # return HttpResponse('Hello, world!')
