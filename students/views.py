ac02f65ce199e0bcb84a8aa1b2d5653c6816dc55
msdsn <devsanms@outlook.com>
2024-03-16 17:03:28 +0300

    Farklı data tiplerini Context'te kullan

--- students/views.py
+++ students/views.py
@@ -4,6 +4,18 @@ from django.http import HttpResponse
 def home(request):
     context = {
         'title': 'Home',
+        'description': 'Welcome to the home page!',
+        'pageNum': '1',
+        'authorsList': [
+            "Author1",
+            "Author2",
+            "Author3",
+        ],
+        'authorsDict': {
+            "key1": 1,
+            "key2": 2,
+        }
+
     }
     return render(request, 'students/home.html', context)
     # return HttpResponse('Hello, world!')
