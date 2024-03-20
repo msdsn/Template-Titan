930ed7a50de01ba31c18e54a333583590b86b024
msdsn <devsanms@outlook.com>
2024-03-20 14:57:43 +0300

    django forms, crud yap

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home, student_list
+from .views import home, student_list, student_add, student_edit, student_detail, student_delete
 
 urlpatterns = [
     path('', home),
-    path('students/', student_list),
+    path('students/', student_list, name='student-list'),
+    path('students/add/', student_add),
+    path('students/edit/<int:id>/', student_edit),
+    path('students/detail/<int:id>/', student_detail),
+    path('students/delete/<int:id>/', student_delete),
 ]
--- students/views.py
+++ students/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Student
+from .forms import StudentForm
 
 def home(request):
     context = {
@@ -31,4 +32,53 @@ def student_list(request):
         'pageNum': '2',
     }
     return render(request, 'students/student_list.html', context)
+    # return HttpResponse('Hello, world!')
+
+def student_add(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        print('Printing POST:', request.POST)
+        print('Printing FILES:', request.FILES)
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('student-list')
+    context = {
+        'form': form,
+    }
+    return render(request, 'students/student_add.html', context)
+    # return HttpResponse('Hello, world!')
+
+
+def student_edit(request, id):
+    student = get_object_or_404(Student, id=id)
+    form = StudentForm(instance=student)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student-list')
+    context = {
+        'form': form,
+    }
+    return render(request, 'students/student_edit.html', context)
+    # return HttpResponse('Hello, world!')
+
+def student_detail(request, id):
+    student = get_object_or_404(Student, id=id)
+    context = {
+        'student': student,
+    }
+    return render(request, 'students/student_detail.html', context)
+    # return HttpResponse('Hello, world!')
+
+def student_delete(request, id):
+    student = get_object_or_404(Student, id=id)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student-list')
+    context = {
+        'student': student,
+    }
+    return render(request, 'students/student_delete.html', context)
     # return HttpResponse('Hello, world!')
